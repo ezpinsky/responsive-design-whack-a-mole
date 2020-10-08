3e70b0d78ef2f984b333af490ef9d61778f03357
carlos molero <cmolero130@gmail.com>
2020-10-08 16:26:59 -0400

    stuck on phase 2

--- mole.js
+++ mole.js
@@ -6,3 +6,19 @@
 //         }
 //     }, 1000);
 // });
+
+const popUpRandomMole = () => {
+    let moleHeads = document.querySelectorAll("wgs__dirt-pile");
+    console.log(moleHeads);
+    let rand = Math.floor(Math.random() * 7);
+    let mole = moleHeads[rand];
+    mole.classList.remove("wgs__mole-head--hidden");
+    setTimeout(hideMole(mole), 1000);
+};
+
+const hideMole = function (mole) {
+    mole.classList.add("wgs__mole-head--hidden");
+    setTimeout(popUpRandomMole, 1000);
+};
+
+window.addEventListener("DOMContentLoaded", popUpRandomMole());
